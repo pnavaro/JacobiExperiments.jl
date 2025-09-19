f171bc5a1336d5a70ae5a95f2600b4f95ed6aab7
Jean-Christophe <loiseau.jc@gmail.com>
2025-09-19 14:52:42 +0200

    Jacobi kernels

--- app/main.f90
+++ app/main.f90
@@ -1,6 +1,55 @@
 program main
-  use Jacobi_Experiments, only: say_hello
-  implicit none
+   use omp_lib
+   use stdlib_constants, only: pi => pi_dp
+   use stdlib_linalg_constants, only: ilp, dp, lk
+   use stdlib_math, only: linspace
+   use stdlib_io_npy, only: save_npy
+   use Jacobi_Experiments
+   implicit none
+   integer(ilp), parameter :: nx = 512, ny = nx, maxiter = nx**2
+   real(dp), dimension(nx, ny) :: b
+   real(dp), dimension(nx, ny) :: u
+   real(dp), dimension(nx) :: x
+   real(dp), dimension(ny) :: y
+   integer(ilp) :: i, j, k
+   real(dp) :: start_time, end_time
 
-  call say_hello()
+   !----- Initialize variables -----!
+   b = 0.0_dp; u = 0.0_dp
+   x = linspace(0, 1, nx); y = linspace(0, 1, ny)
+
+   ! Create right-hand side vector.
+   do concurrent(i=1:nx, j=1:ny)
+      b(i, j) = sin(2*pi*x(i))*sin(2*pi*y(j))
+   end do
+
+   call save_npy("rhs_vector.npy", b)
+
+   !----------------------------------
+   !-----     JACOBI SOLVERS     -----
+   !----------------------------------
+
+   !> Textbook Jacobi solver.
+   ! call cpu_time(start_time)
+   start_time = omp_get_wtime()
+   u = textbook_solver(b, maxiter)
+   end_time = omp_get_wtime()
+   ! call cpu_time(end_time)
+   print *, "Textbook solver : ", end_time - start_time
+   call save_npy("textbook_solution.npy", u)
+
+   !> flipflop Jacobi solver.
+   start_time = omp_get_wtime()
+   u = flipflop_solver(b, maxiter)
+   end_time = omp_get_wtime()
+   print *, "flipflop solver :", end_time - start_time
+   call save_npy("flipflop_solution.npy", u)
+
+   !> doconcurrent Jacobi solver.
+   start_time = omp_get_wtime()
+   u = doconcurrent_solver(b, maxiter)
+   end_time = omp_get_wtime()
+   print *, "doconccurent solver :", end_time - start_time
+   call save_npy("doconcurrent_solution.npy", u)
+
 end program main
--- src/Jacobi-Experiments.f90
+++ src/Jacobi-Experiments.f90
@@ -1,10 +1,141 @@
 module Jacobi_Experiments
-  implicit none
-  private
+   use stdlib_linalg_constants, only: dp, ilp, lk
+   implicit none(external)
+   private
 
-  public :: say_hello
+   interface
+      module function textbook_solver(b, maxiter) result(u)
+         implicit none(external)
+         real(dp), intent(in) :: b(:, :)
+         integer(ilp), intent(in) :: maxiter
+         real(dp), allocatable :: u(:, :)
+      end function textbook_solver
+
+      module function doconcurrent_solver(b, maxiter) result(u)
+         implicit none(external)
+         real(dp), intent(in) :: b(:, :)
+         integer(ilp), intent(in) :: maxiter
+         real(dp), allocatable :: u(:, :)
+      end function doconcurrent_solver
+
+      module function flipflop_solver(b, maxiter) result(u)
+         implicit none(external)
+         real(dp), intent(in) :: b(:, :)
+         integer(ilp), intent(in) :: maxiter
+         real(dp), allocatable :: u(:, :)
+      end function flipflop_solver
+   end interface
+
+   public :: textbook_solver
+   public :: doconcurrent_solver
+   public :: flipflop_solver
+
 contains
-  subroutine say_hello
-    print *, "Hello, Jacobi-Experiments!"
-  end subroutine say_hello
+
+   !----------------------------------
+   !-----     JACOBI SOLVERS     -----
+   !----------------------------------
+
+   module function textbook_solver(b, maxiter) result(u)
+      implicit none(external)
+      real(dp), intent(in) :: b(:, :)
+      integer(ilp), intent(in) :: maxiter
+      real(dp), allocatable :: u(:, :)
+      ! Internal variables.
+      integer(ilp) :: nx, ny, i, j, iteration
+      real(dp), allocatable :: v(:, :)
+      real(dp) :: dx
+
+      ! Initialize variables
+      nx = size(b, 1); ny = size(b, 2); dx = 1.0_dp/(nx - 1)
+      if (nx /= ny) error stop "Number of points in each direction need to be equal."
+      allocate (u(nx, ny), source=0.0_dp)
+      allocate (v(nx, ny), source=0.0_dp)
+
+      do iteration = 1, maxiter
+         ! Jacobi iteration.
+         call textbook_kernel(nx, ny, v, u, b, dx)
+         ! Update variable.
+         u = v
+      end do
+   end function
+
+   module function flipflop_solver(b, maxiter) result(u)
+      implicit none(external)
+      real(dp), intent(in) :: b(:, :)
+      integer(ilp), intent(in) :: maxiter
+      real(dp), allocatable :: u(:, :)
+      ! Internal variables.
+      integer(ilp) :: nx, ny, i, j, iteration
+      real(dp), allocatable :: v(:, :)
+      real(dp) :: dx
+
+      ! Initialize variables
+      nx = size(b, 1); ny = size(b, 2); dx = 1.0_dp/(nx - 1)
+      if (nx /= ny) error stop "Number of points in each direction need to be equal."
+      allocate (u(nx, ny), source=0.0_dp)
+      allocate (v(nx, ny), source=0.0_dp)
+
+      do iteration = 1, maxiter, 2
+         ! Jacobi kernel.
+         call textbook_kernel(nx, ny, v, u, b, dx)
+         ! Update variables.
+         call textbook_kernel(nx, ny, u, v, b, dx)
+      end do
+   end function
+
+   module function doconcurrent_solver(b, maxiter) result(u)
+      implicit none(external)
+      real(dp), intent(in) :: b(:, :)
+      integer(ilp), intent(in) :: maxiter
+      real(dp), allocatable :: u(:, :)
+      ! Internal variables.
+      integer(ilp) :: nx, ny, i, j, iteration
+      real(dp), allocatable :: v(:, :)
+      real(dp) :: dx
+
+      ! Initialize variables
+      nx = size(b, 1); ny = size(b, 2); dx = 1.0_dp/(nx - 1)
+      if (nx /= ny) error stop "Number of points in each direction need to be equal."
+      allocate (u(nx, ny), source=0.0_dp)
+      allocate (v(nx, ny), source=0.0_dp)
+
+      do iteration = 1, maxiter, 2
+         ! Jacobi kernel.
+         call doconcurrent_kernel(nx, ny, v, u, b, dx)
+         ! Update variables.
+         call doconcurrent_kernel(nx, ny, u, v, b, dx)
+      end do
+   end function
+
+   !----------------------------------
+   !-----     JACOBI KERNELS     -----
+   !----------------------------------
+
+   pure subroutine textbook_kernel(nx, ny, u, v, b, dx)
+      implicit none(external)
+      integer(ilp), intent(in) :: nx, ny
+      real(dp), intent(out) :: u(nx, ny)
+      real(dp), intent(in) :: v(nx, ny), b(nx, ny), dx
+      integer(ilp) :: i, j
+      do j = 2, ny - 1
+         do i = 2, nx - 1
+            u(i, j) = 0.25_dp*(b(i, j)*dx**2 - v(i + 1, j) - v(i - 1, j) &
+                               - v(i, j + 1) - v(i, j - 1))
+         end do
+      end do
+   end subroutine textbook_kernel
+
+   pure subroutine doconcurrent_kernel(nx, ny, u, v, b, dx)
+      implicit none(external)
+      integer(ilp), intent(in) :: nx, ny
+      real(dp), intent(out) :: u(nx, ny)
+      real(dp), intent(in) :: v(nx, ny), b(nx, ny), dx
+      integer(ilp) :: i, j
+      do concurrent(i=2:nx - 1, j=2:ny - 1)
+         u(i, j) = 0.25_dp*(b(i, j)*dx**2 - v(i + 1, j) - v(i - 1, j) &
+                            - v(i, j + 1) - v(i, j - 1))/4
+      end do
+   end subroutine doconcurrent_kernel
+
 end module Jacobi_Experiments
